6e882aba58636fd505486593459299026c3a22a4
时光之末 <chronosmaker@126.com>
2021-05-24 11:50:19 +0800

    盛最多水的容器

---
+++ 11.盛最多水的容器.js
@@ -0,0 +1,34 @@
+/*
+ * @lc app=leetcode.cn id=11 lang=javascript
+ *
+ * [11] 盛最多水的容器
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+var maxArea = function (height) {
+  let max = 0;
+  for (let i = 0, j = height.length - 1; i < j; ) {
+    const y = height[i] < height[j] ? height[i++] : height[j--];
+    const x = j - i + 1;
+    max = Math.max(max, x * y);
+  }
+  return max;
+};
+// @lc code=end
+
+//   // timeout;
+//   let max = 0;
+//   const len = height.length;
+//   for (let i = 0; i < len - 1; i++) {
+//     for (let j = i + 1; j < len; j++) {
+//       const x = j - i;
+//       const y = height[i] < height[j] ? height[i] : height[j];
+//       const res = x * y;
+//       max = max > res ? max : res;
+//     }
+//   }
+//   return max;
