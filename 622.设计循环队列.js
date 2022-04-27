f6b07bccc48ebdb02303ff44af119f1f815e0b2f
chronosmaker <chronosmaker@126.com>
2022-04-27 20:53:32 +0800

    设计循环队列

---
+++ 622.设计循环队列.js
@@ -0,0 +1,82 @@
+/*
+ * @lc app=leetcode.cn id=622 lang=javascript
+ *
+ * [622] 设计循环队列
+ */
+
+// @lc code=start
+/**
+ * @param {number} k
+ */
+var MyCircularQueue = function (k) {
+  this.arr = new Array(k);
+  this.size = k;
+  this.cnt = 0;
+  this.head = 0;
+  this.tail = 0;
+};
+
+/**
+ * @param {number} value
+ * @return {boolean}
+ */
+MyCircularQueue.prototype.enQueue = function (value) {
+  if (this.isFull()) return false;
+  this.arr[this.tail] = value;
+  this.cnt++;
+  this.tail = (this.tail + 1) % this.size;
+  return true;
+};
+
+/**
+ * @return {boolean}
+ */
+MyCircularQueue.prototype.deQueue = function () {
+  if (this.isEmpty()) return false;
+  this.arr[this.head] = null;
+  this.cnt--;
+  this.head = (this.head + 1) % this.size;
+  return true;
+};
+
+/**
+ * @return {number}
+ */
+MyCircularQueue.prototype.Front = function () {
+  if (this.isEmpty()) return -1;
+  return this.arr[this.head];
+};
+
+/**
+ * @return {number}
+ */
+MyCircularQueue.prototype.Rear = function () {
+  if (this.isEmpty()) return -1;
+  return this.arr[(this.tail || this.size) - 1];
+};
+
+/**
+ * @return {boolean}
+ */
+MyCircularQueue.prototype.isEmpty = function () {
+  return this.cnt === 0;
+};
+
+/**
+ * @return {boolean}
+ */
+MyCircularQueue.prototype.isFull = function () {
+  return this.cnt === this.size;
+};
+
+/**
+ * Your MyCircularQueue object will be instantiated and called as such:
+ * var obj = new MyCircularQueue(k)
+ * var param_1 = obj.enQueue(value)
+ * var param_2 = obj.deQueue()
+ * var param_3 = obj.Front()
+ * var param_4 = obj.Rear()
+ * var param_5 = obj.isEmpty()
+ * var param_6 = obj.isFull()
+ */
+// @lc code=end
